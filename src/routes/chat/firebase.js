a740942d60be6546238f982c3c598c9bc97791db
Muhammad Ilham Jaya <m.ilhamjaya1808@gmail.com>
2024-02-27 07:32:11 +0700

    4

--- src/routes/chat/firebase.js
+++ src/routes/chat/firebase.js
@@ -1,19 +1,21 @@
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
- const firebaseConfig = {
-    apiKey: "AIzaSyAwEmQH_HS52Fh0v0rr_Sca_b_W0fTI6qI",
-    authDomain: "svelte-porto.firebaseapp.com",
-    projectId: "svelte-porto",
-    storageBucket: "svelte-porto.appspot.com",
-    messagingSenderId: "322582306724",
-    appId: "1:322582306724:web:5ea81582467298fbde6b31",
-    measurementId: "G-8RWXPG85C5"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAwEmQH_HS52Fh0v0rr_Sca_b_W0fTI6qI",
+  authDomain: "svelte-porto.firebaseapp.com",
+  projectId: "svelte-porto",
+  storageBucket: "svelte-porto.appspot.com",
+  messagingSenderId: "322582306724",
+  appId: "1:322582306724:web:5ea81582467298fbde6b31",
+  measurementId: "G-8RWXPG85C5"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-export const googleProvider = new firebase.auth.GoogleAuthProvider();
-export const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
